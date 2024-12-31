5dba8d375fe88dc55ea24ed67cb3c7d4bbdf1c61
Vladissluff <vlad.bomboman@gmail.com>
2024-12-31 17:55:34 +0300

    gh01

--- js/taks-1.js
+++ js/taks-1.js
@@ -1,7 +1,7 @@
 function makeTransaction(quantity, pricePerDroid) {
     const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${pricePerDroid} credits!`;
-    
+    const message = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    return message;
 }
 
 console.log(makeTransaction(5, 3000)); 
